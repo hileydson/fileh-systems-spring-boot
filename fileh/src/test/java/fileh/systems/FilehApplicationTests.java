fc1d9c9ea5aff499a2e4a2c500bd77b101a518a2
Hileydson Cogo <hileydson@hotmail.com>
2022-08-19 11:31:05 -0300

    Teste comentado e pagina html de teste

--- fileh/src/test/java/fileh/systems/FilehApplicationTests.java
+++ fileh/src/test/java/fileh/systems/FilehApplicationTests.java
@@ -1,13 +1,13 @@
-package fileh.systems;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FilehApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package fileh.systems;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FilehApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
